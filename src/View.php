b578812b50ed01e84a828df70cd4de00088b86db
ADRIANO JUNIOR <adriano.santos.junior@gmail.com>
2014-12-11 21:37:46 -0200

    Parvus View

--- src/View.php
+++ src/View.php
@@ -6,6 +6,12 @@
 
         public final function render ($prView,$prArray = array())
         {
+            /** If cache folder not exists, create this */
+            if (!is_dir(path.'app/cache'))
+            {
+                mkdir(path.'app/cache',0775,true);
+            }
+
             $blade = new \Philo\Blade\Blade(path.'app/view', path.'app/cache');
 
             return $blade->view()->make($prView,$prArray)->render();
