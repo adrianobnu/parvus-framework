d6758882c455b074d372272d167ebd1a7c64efb2
Adriano dos Santos Júnior <adriano.santos@w2o.com.br>
2015-06-09 11:28:48 -0300

    Size de inputs FILES

--- src/Input.php
+++ src/Input.php
@@ -14,7 +14,20 @@
 
 			$aFile = $_FILES[$prName];
 
+			/**
+			 * Get the extension of the file
+			 */
 			$aFile['extension'] = strToLower(pathinfo($aFile['name'], PATHINFO_EXTENSION));
+
+			/**
+			 * Convert the size
+			 */
+			$aFile['size'] = array (
+				'byte'	   => $aFile['size'],
+				'kilobyte' => $aFile['size'] * 1024,
+				'megabyte' => $aFile['size'] * 2048,
+				'gigabyte' => $aFile['size'] * 4096
+			);
 
 			return $aFile;
 		}
