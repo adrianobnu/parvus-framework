bedf961893c6dcd8ab3bc9898adb706f4c11d9fe
Adriano dos Santos Júnior <adriano.santos@w2o.com.br>
2015-06-09 11:37:33 -0300

    Size de inputs FILES

--- src/Input.php
+++ src/Input.php
@@ -23,10 +23,10 @@
 			 * Convert the size
 			 */
 			$aFile['size'] = array (
-				'byte'	   => $aFile['size'],
-				'kilobyte' => $aFile['size'] * 1024,
-				'megabyte' => $aFile['size'] * 2048,
-				'gigabyte' => $aFile['size'] * 4096
+				'byte'	   => number_format($aFile['size'], 2),
+				'kilobyte' => number_format($aFile['size'] / 1024, 2),
+				'megabyte' => number_format($aFile['size'] / 1048576, 2),
+				'gigabyte' => number_format($aFile['size'] / 1073741824, 2)
 			);
 
 			return $aFile;
