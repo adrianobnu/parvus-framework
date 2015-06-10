f468461d8cc4bafabc0915560409b0a60ee18c2e
Adriano dos Santos Júnior <adriano.santos@w2o.com.br>
2015-06-10 15:05:55 -0300

    Date

--- src/Date.php
+++ src/Date.php
@@ -4,6 +4,81 @@
     class Date
     {
 
+		/**
+		 * Sub a data and return then
+		 * @param $prDate string Date in format Y-m-d or Ymd (If null, use the actual date)
+		 * @param $prQuantity number Quantity to sub
+		 * @param string $prType string Type (d,m,y)
+		 * @param string $prFormat string Output format of the date
+		 * @return string
+		 */
+		public static final function sub ($prDate, $prQuantity, $prType = 'd', $prFormat = 'Y-m-d')
+		{
+
+			return Date::addSub($prDate,$prQuantity,$prType,$prFormat,'sub');
+
+		}
+
+		/**
+		 * Add a data and return then
+		 * @param $prDate string Date in format Y-m-d or Ymd (If null, use the actual date)
+		 * @param $prQuantity number Quantity to add
+		 * @param string $prType string Type (d,m,y)
+		 * @param string $prFormat string Output format of the date
+		 * @return string
+		 */
+		public static final function add ($prDate, $prQuantity, $prType = 'd', $prFormat = 'Y-m-d')
+		{
+
+			return Date::addSub($prDate,$prQuantity,$prType,$prFormat,'add');
+
+		}
+
+		/**
+		 * Add a data and return then
+		 * @param $prDate string Date in format Y-m-d or Ymd (If null, use the actual date)
+		 * @param $prQuantity number Quantity to add/sub
+		 * @param $prType string Type (d,m,y)
+		 * @param $prFormat string Output format of the date
+		 * @param $prAction string Action (add,sub)
+		 * @return string
+		 */
+		private static final function addSub ($prDate, $prQuantity, $prType , $prFormat, $prAction)
+		{
+
+			$time		= strToTime($prDate != NULL ? $prDate : date('Y-m-d'));
+			$day 		= date('d',$time);
+			$month 		= date('m',$time);
+			$year 		= date('Y',$time);
+			$prAction 	= strToLower($prAction);
+
+			switch (strToLower($prType))
+			{
+
+				case 'd' :
+				{
+					$day = ($prAction == 'sub' ? $day -= $prQuantity : $day += $prQuantity);
+					break;
+				}
+
+				case 'y' :
+				{
+					$year = ($prAction == 'sub' ? $year -= $prQuantity : $year += $prQuantity);
+					break;
+				}
+
+				case 'm' :
+				{
+					$month = ($prAction == 'sub' ? $month -= $prQuantity : $month += $prQuantity);
+					break;
+				}
+
+			}
+
+			return date($prFormat, mktime (0, 0, 0, $month, $day, $year));
+
+		}
+
         public static final function show ($prDate)
         {
             return $prDate ? date ('d/m/Y'.(strpos($prDate,':') !== false ? ' H:i:s' : NULL),strToTime($prDate)) : NULL;
