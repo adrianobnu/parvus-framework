fbb417eafee15cae3fbb2f16384794f2b8b40db5
Adriano dos Santos Júnior <adriano@adrianojr.com>
2016-03-01 16:04:10 -0300

    Update Header.php

--- src/Header.php
+++ src/Header.php
@@ -4,6 +4,29 @@
     class Header
     {
 
+        public static function pdf ()
+        {
+            header('Cache-Control: no-cache, must-revalidate');
+            header('Expires: Mon, 26 Jul 1997 05:00:00 GMT');
+            header('Content-type: application/pdf');
+        }
+
+        public final static function CSV ($prFileName)
+        {
+
+            if (mb_substr(mb_strtolower($prFileName,'UTF-8'),-4) != '.csv')
+            {
+
+                $prFileName.= '.csv';
+
+            }
+
+            header('Cache-Control: no-cache, must-revalidate');
+            header('Expires: Mon, 26 Jul 1997 05:00:00 GMT');
+            header('Content-type: text/csv');
+            header('Content-Disposition: attachment; filename="'.$prFileName);
+        }
+
         public final static function JSON ($prJSON = NULL)
         {
             header('Content-Type: application/json');
