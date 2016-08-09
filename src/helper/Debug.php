a4a4b46d287d193bef268839649fd19e6a14c792
Adriano dos Santos Júnior <adriano.santos.jr@w2o.com.br>
2016-08-09 15:52:45 -0300

    Debug

--- src/helper/Debug.php
+++ src/helper/Debug.php
@@ -1,26 +1,36 @@
 <?php
-
     /**
      * @param $prItem
+     * @param bool $prExit
      */
-    function pre_print_r ($prItem)
+    function pre_print_r ($prItem, $prExit = true)
     {
 
         print('<pre>');
             print_r($prItem);
         print('</pre>');
 
+        if ($prExit)
+        {
+            exit;
+        }
+
     }
 
     /**
-     *
      * @param $prItem
+     * @param bool $prExit
      */
-    function pre_var_dump ($prItem)
+    function pre_var_dump ($prItem, $prExit = true)
     {
 
         print('<pre>');
             var_dump($prItem);
         print('</pre>');
+
+        if ($prExit)
+        {
+            exit;
+        }
 
     }
