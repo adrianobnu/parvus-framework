ba05cea875d27136dedadfb01fce509c6934e0ec
Adriano dos Santos Júnior <adriano.santos.junior@gmail.com>
2016-12-08 01:36:03 -0200

    Add Model Base

--- src/Model.php
+++ src/Model.php
@@ -12,6 +12,14 @@
             parent::__construct();
             parent::boot();
 
+            /** Se não tem tabela, recupera pelo nome da classe */
+            if ($this->table == NULL)
+            {
+
+                $this->table = mb_strtolower(explode("\\",get_called_class())[1],'UTF-8');
+
+            }
+
             /** When save, remove NULL fields */
             static::saving(function($model)
             {
@@ -31,6 +39,37 @@
                 return true;
 
             });
+
+        }
+
+        /**
+         * Retorna um objeto pelo ID
+         * @param $prId
+         * @param $prCampo
+         * @return mixed
+         */
+        public final static function getById ($prId,$prCampo = 'id')
+        {
+
+            /** Gera o nome da model */
+            $model = 'Model\\'.(explode('\\',get_called_class())[1]);
+
+            /** Se existir o model, criar o objeto do model */
+            if ($prId != NULL && class_exists($model))
+            {
+
+                $value = $model::where($prCampo,$prId)->first();
+
+                if ($value != NULL)
+                {
+
+                    return $value;
+
+                }
+
+            }
+
+            return new $model();
 
         }
 
