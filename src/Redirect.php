570dfe3ccefb0cbec480750fa5603c1c2d25eac5
Adriano dos Santos Júnior <adriano.santos.junior@gmail.com>
2016-12-08 01:21:15 -0200

    Add Model Base

--- src/Redirect.php
+++ src/Redirect.php
@@ -5,24 +5,15 @@
 	{
 
         /**
-         * @return mixed
+         * @param null $prURL
          */
-        public final static function withInput ()
-        {
+		public final static function to ($prURL = NULL)
+		{
 
             unset ($_SESSION[PARVUS_FLASH_SESSION_NAME]);
 
             $_SESSION[PARVUS_FLASH_SESSION_NAME] = $_POST;
 
-            return self::class;
-
-        }
-
-        /**
-         * @param null $prURL
-         */
-		public final static function to ($prURL = NULL)
-		{
 			$url = str_replace(DIRECTORY_SEPARATOR,'/',url.$prURL);
 
 			if (substr($url,-1) == '/')
