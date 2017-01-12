1da018f828a7e5c9960d07c28e6264b7ed7e759b
Adriano dos Santos Júnior <contato@appeardigital.com.br>
2017-01-12 13:18:02 -0200

    Update View.php

--- src/View.php
+++ src/View.php
@@ -13,14 +13,14 @@
         {
             
             /** If cache folder not exists, create this */
-            if (!is_dir(path.'app/cache'))
+            if (!is_dir(path.'vendor/cache'))
             {
 
-                mkdir(path.'app/cache',0775,true);
+                mkdir(path.'vendor/cache',0775,true);
 
             }
 
-            $blade = new \Philo\Blade\Blade(path.'app/view', path.'app/cache');
+            $blade = new \Philo\Blade\Blade(path.'app/view', path.'vendor/cache');
 
             return $blade->view()->make($prView,$prArray)->render();
 
