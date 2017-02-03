da5aa181995be56547ff8ea9a6de63b28e62ac70
Adriano dos Santos Júnior <adriano.santos.jr@w2o.com.br>
2017-02-03 10:34:26 -0200

    Adicionado helper para textos

--- src/helper/Text.php
+++ src/helper/Text.php
@@ -1,28 +1,16 @@
 <?php
     /**
-     * @param $string
-     * @param bool $line_breaks
-     * @param bool $xml
+     * @param $prString
      * @return string
      */
-    function nl2p($string, $line_breaks = true, $xml = true)
+    function nl2p($prString)
     {
 
-        $string = str_replace(array('<p>', '</p>', '<br>', '<br />'), '', $string);
-
-        if ($line_breaks == true)
+        if ($prString != NULL)
         {
 
-            return '<p>'.preg_replace(array("/([\n]{2,})/i", "/([^>])\n([^<])/i"), array("</p>\n<p>", '$1<br'.($xml == true ? ' /' : '').'>$2'), trim($string)).'</p>';
+            return '<p>'.str_replace(array('<br>','<br />'),'<p>',nl2br(preg_replace("/[\r\n]+/", "\n", $prString)));
 
         }
-        else
-        {
 
-            return '<p>'.preg_replace (
-                    array("/([\n]{2,})/i", "/([\r\n]{3,})/i","/([^>])\n([^<])/i"),
-                    array("</p>\n<p>", "</p>\n<p>", '$1<br'.($xml == true ? ' /' : '').'>$2'),
-                    trim($string)).'</p>';
-
-        }
     }
