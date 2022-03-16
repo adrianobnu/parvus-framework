b938020599b1bc86f7365ccb4f6920cfc76ec8f2
Adriano dos Santos Júnior <adriano.santos.junior@gmail.com>
2022-03-16 20:22:05 +0000

    Update View.php

--- src/View.php
+++ src/View.php
@@ -10,20 +10,10 @@
          * @return mixed
          */
         public final function render ($prView,$prArray = array())
-        {
-            
-            /** If cache folder not exists, create this */
-            if (!is_dir(path.'vendor/cache'))
-            {
-
-                mkdir(path.'vendor/cache',0775,true);
-
-            }
-
-            $blade = new \Philo\Blade\Blade(path.'app/view', path.'vendor/cache');
+        {            
+            $blade = new \Philo\Blade\Blade(path.'app/view', path.'cache');
 
             return $blade->view()->make($prView,$prArray)->render();
-
         }
 
     }
