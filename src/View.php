73ef5d6278d2b00b751351f5ef78574ccb1750bc
Adriano Junior <adriano.santos.junior@gmail.com>
2024-04-11 18:12:49 -0300

    Update View.php

--- src/View.php
+++ src/View.php
@@ -11,6 +11,10 @@
          */
         public final function render ($prView,$prArray = array())
         {            
+			if (!is_dir(path . 'vendor/.cache')) {
+	            mkdir(path . 'vendor/.cache', 0755, true);
+	        }
+			
             $blade = new \Philo\Blade\Blade(path.'app/view', path.'vendor/.cache');
 
             return $blade->view()->make($prView,$prArray)->render();
